eb7413facce21aad64ffeeb0ece1ba3ea292704f
Andrew Scherer <ascherer97@gmail.com>
2025-06-09 17:15:40 -0700

    publishing package to pypi and adding ci infrastructure

--- emu_utilities/__init__.py
+++ emu_utilities/__init__.py
@@ -1,3 +1,5 @@
+import importlib.metadata
+
 from .adjoint_gradient import load_adjoint_gradient
 from .attribution import load_attribution
 from .convolution import control_variance, lagged_variance, load_1d_convolution, load_2d_convolution, spatial_variance
@@ -17,3 +19,8 @@ __all__ = [
     "load_sample",
     "load_tracer",
 ]
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"  # Fallback for development mode
